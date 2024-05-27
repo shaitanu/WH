19cfe4138eb7b56c9f2650e9a7fc6c10e1eb0ca9
Rohit Kotha <rohitkotha10@gmail.com>
2024-05-28 00:24:06 +0530

    Home page modifications

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -1,9 +1,7 @@
 'use client'
 import { Link } from '@/src/navigation'
 import { FC } from 'react'
-import LangSwitcher from './LangSwitcher'
 import Image from 'next/image'
-import ThemeSwitch from './ThemeSwitch'
 interface Props {
   locale: string
 }
@@ -12,27 +10,16 @@ export const Header: FC<Props> = ({ locale }) => {
     <div className='mx-auto flex max-w-screen-2xl flex-row items-center justify-between p-5'>
       <Link lang={locale} href='/'>
         <div className='flex flex-row items-center'>
-            <Image src="/images/logo.png" alt="Logo" width={100} height={100} />
+          <Image src="/images/logo.png" alt="Logo" width={100} height={100} />
 
         </div>
       </Link>
       <div className='flex flex-row items-center gap-3'>
         <nav className='mr-10 inline-flex gap-5'>
           <Link lang={locale} href={`/about`}>
-            {'About'}
+            About Us
           </Link>
-          <a href=''>{'Support'}</a>
-          <a href=''>{'Other'}</a>
         </nav>
-        <ThemeSwitch />
-        <LangSwitcher />
-        <a
-          href='https://github.com/yahyaparvar/nextjs-template'
-          target='_blank'
-        >
-          <div className='size-8'>
-          </div>
-        </a>
       </div>
     </div>
   )
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,71 +1,52 @@
-import { useTranslations } from 'next-intl'
 import Button from './components/Button'
 
 export default function DashboardPage() {
-  const t = useTranslations('')
   return (
     <div>
       <section className='flex flex-col items-center justify-center py-24'>
         <h1 className='text-center text-7xl font-extrabold leading-tight'>
-          {t('An')}{' '}
           <span className='bg-span-bg bg-clip-text text-transparent'>
-            {t('Booster')}
+            W&H
           </span>
           <br />
-          {t('to_Your_NextJS_Apps')}
+          Eudaimonia
         </h1>
         <div className='my-6 px-20 text-center text-2xl text-text-secondary'>
-          {t(
-            'An_approachable_performant_and_versatile_boilerplate_for_building_SSR_applications'
-          )}
+          Mental Health Made Easy ...
         </div>
         <div className='mt-4 flex flex-row gap-4'>
           <a
-            href='https://github.com/new?template_name=nextjs-template&template_owner=yahyaparvar'
-            target='_blank'
-          >
-            <Button rounded size='large'>
-              {t('Use_Template')}
-            </Button>
-          </a>
-          <a
-            href='https://github.com/yahyaparvar/nextjs-template'
-            target='_blank'
-          >
+            href="/pitch.pdf"
+            target="_blank"
+            rel="noopener noreferrer">
             <Button rounded size='large' variant='secondary'>
-              {t('Learn_More')}
+              Learn More!
             </Button>
           </a>
         </div>
-      </section>
+      </section >
       <section className='bg-background-secondary py-20 max-lg:py-10'>
         <div className='mx-auto grid max-w-screen-lg grid-cols-3 gap-7 px-8 py-5 max-lg:max-w-fit max-lg:grid-cols-1 max-lg:gap-10'>
           <div className='text-center'>
-            <h2 className='mb-3  text-xl font-semibold'>{t('Approachable')}</h2>
+            <h2 className='mb-3  text-xl font-semibold'>Approachable</h2>
             <p className='text-text-secondary max-lg:max-w-[500px]'>
-              {t(
-                'Add_components_without_sending_additional_client_side_JavaScript_Built_on_the_latest_React_features'
-              )}
+              Add compo Lorem Ipsum
             </p>
           </div>
           <div className='text-center'>
-            <h2 className='mb-3 text-xl font-semibold'>{t('Versatile')}</h2>
+            <h2 className='mb-3 text-xl font-semibold'>Versatile</h2>
             <p className='text-text-secondary max-lg:max-w-[500px]'>
-              {t(
-                'Automatic_Image_Font_and_Script_Optimizations_for_improved_UX_and_Core_Web_Vitals'
-              )}
+              Approachable Lorem Ipsum
             </p>
           </div>
           <div className='text-center'>
-            <h2 className='mb-3 text-xl font-semibold'>{t('Performant')}</h2>
+            <h2 className='mb-3 text-xl font-semibold'>Performat</h2>
             <p className='text-text-secondary max-lg:max-w-[500px]'>
-              {t(
-                'A_rich_incredibly_adoptable_template_that_scales_between_a_small_showcase_website_and_a_full_size_app'
-              )}
+              Rich Incredible Lorem Ipsum
             </p>
           </div>
         </div>
       </section>
-    </div>
+    </div >
   )
 }
